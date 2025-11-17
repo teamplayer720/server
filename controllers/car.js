392ab779db35d0c6564b2db602e94e58dfa3337e
Christian Boswell <97624302+teamplayer720@users.noreply.github.com>
2025-11-17 11:04:24 -0600

    update

--- controllers/car.js
+++ controllers/car.js
@@ -129,6 +129,21 @@ exports.car_view_one_Page = async function(req, res) {
     res.status(500)
     res.send(`{'error': '${err}'}`);
     }
+}; 
+
+ //Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.car_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+        res.render('carcreate', { title: 'Car Create'});
+    }
+catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
 };
+
 
 
--- routes/car.js
+++ routes/car.js
@@ -5,6 +5,9 @@ var router = express.Router();
 router.get('/', car_controllers.car_view_all_Page); 
 
 /* GET detail costume page */
-router.get('/detail', car_controllers.car_view_one_Page);
+router.get('/detail', car_controllers.car_view_one_Page); 
+
+/* GET create costume page */
+router.get('/create', car_controllers.car_create_Page);
 
 module.exports = router;
