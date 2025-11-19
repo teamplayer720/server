7d86f9312d680d1a22242c7c4b0e39260f77eb76
Christian Boswell <97624302+teamplayer720@users.noreply.github.com>
2025-11-19 17:16:46 -0600

    Update car.js

--- controllers/car.js
+++ controllers/car.js
@@ -1,9 +1,9 @@
-var Car = require('../models/car');
+var car = require('../models/car');
 // List of all Costumes
 exports.car_list = async function(req, res) {
     try{
-        theCars = await Car.find();
-        res.send(theCars);
+        thecars = await car.find();
+        res.send(thecars);
     }
         catch(err)
         {
@@ -29,7 +29,7 @@ exports.car_update_put = async function(req, res) {
 console.log(`update on id ${req.params.id} with body
 ${JSON.stringify(req.body)}`)
 try {
-let toUpdate = await Car.findById( req.params.id)
+let toUpdate = await car.findById( req.params.id)
 // Do updates of properties
 if(req.body.model)
     toUpdate.model = req.body.model;
@@ -51,8 +51,8 @@ failed`);
 // Handle a show all view
 exports.car_view_all_Page = async function(req, res) {
 try{
-theCars = await Car.find();
-res.render('car', { title: 'Car Search Results', results: theCars });
+thecars = await car.find();
+res.render('car', { title: 'Car Search Results', results: thecars });
 }
 catch(err){
 res.status(500);
@@ -70,7 +70,7 @@ module.exports = router;*/
 // Handle Costume create on POST.
 exports.car_create_post = async function(req, res) {
     console.log(req.body)
-    let document = new Car();
+    let document = new car();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
@@ -92,7 +92,7 @@ exports.car_create_post = async function(req, res) {
 exports.car_detail = async function(req, res) {
 console.log("detail" + req.params.id)
 try {
-result = await Car.findById( req.params.id)
+result = await car.findById( req.params.id)
 res.send(result)
 } catch (error) {
 res.status(500)
@@ -105,7 +105,7 @@ exports.car_delete = async function(req, res) {
     console.log("delete " + req.params.id)
     try {
 
-    result = await Car.findByIdAndDelete( req.params.id)
+    result = await car.findByIdAndDelete( req.params.id)
     console.log("Removed " + result)
     res.send(result)
 
@@ -121,7 +121,7 @@ catch (err) {
 exports.car_view_one_Page = async function(req, res) {
     console.log("single view for id " + req.query.id)
     try{
-        result = await Car.findById( req.query.id)
+        result = await car.findById( req.query.id)
         res.render('car_detail',
             { title: 'Car Detail', toShow: result });
     }
@@ -137,7 +137,7 @@ exports.car_view_one_Page = async function(req, res) {
 exports.car_create_Page = async function(req, res) {
     console.log("create view")
     try{
-        res.render('car_create', { title: 'Car Create'});
+        res.render('car_create', { title: 'car Create'});
     }
 catch(err){
     res.status(500)
@@ -150,7 +150,7 @@ catch(err){
 exports.car_update_Page = async function(req, res) {
     console.log("update view for item "+req.query.id)
     try{
-        let result = await Car.findById(req.query.id)
+        let result = await car.findById(req.query.id)
         res.render('car_update', { title: 'Car Update', toShow: result });
     }
 catch(err){
@@ -164,7 +164,7 @@ catch(err){
 exports.car_delete_Page = async function(req, res) {
     console.log("Delete view for id " + req.query.id)
     try{
-        result = await Car.findById(req.query.id)
+        result = await car.findById(req.query.id)
         res.render('car_delete', { title: 'Car Delete', toShow:
         result });
     }
